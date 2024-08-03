6fa8f1ece3e36d85fd3c72d12df429c5a28c390e
Chattree <shubham.chattree@iqvia.com>
2024-08-03 09:13:02 +0530

    added Home Leftbar Main PostPopup Rightbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Login from "./components/Login";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Leftbar from "./components/Leftbar";
 export default function App () {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login/>}/>
-      <Route path="/login/home" element={<Navbar/>}/>
+      <Route path="/login/home" element={<Home/>}/>
+      
     </Routes>
     </BrowserRouter>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import bell from "../assets/bell.png"
+import globe from "../assets/globe.png"
+import group from "../assets/group.png"
+import clipboard from "../assets/clipboard.png"
+import edit from "../assets/edit.png"
+import home from "../assets/home.png"
+import quora from "../assets/quora.png"
+import lens from "../assets/lens.png"
+import Avatar from "react-avatar"
+// import { auth } from "../firebase/setup"
+// import account from "../assets/account.png"
+import PostPopup from "./PostPopup"
+import { useState } from "react"
 
-const Navbar: React.FC = () => {
-    return (
-        <nav className="bg-white shadow p-4 flex justify-between items-center">
-            <div className="text-2xl font-bold text-red-600">Quora</div>
-            <div className="flex items-center space-x-4">
-                <div className="flex space-x-2">
-                    <button className="bg-gray-100 p-2 rounded">Home</button>
-                    <button className="bg-gray-100 p-2 rounded">Spaces</button>
-                    <button className="bg-gray-100 p-2 rounded">Notifications</button>
-                </div>
-                <input 
-                    type="text" 
-                    placeholder="Search Quora" 
-                    className="border p-2 rounded w-64"
-                />
-                <button className="bg-red-600 text-white p-2 rounded">Add question</button>
-            </div>
-        </nav>
-    );
-};
+type searchProp = {
+  setSearch:any
+}
 
-export default Navbar;
+
+const Navbar = (props:searchProp) => {
+
+  const [post,setPost] = useState(false)
+
+
+  return (
+    <div className="flex pl-20 pt-3 shadow-md h-14 w-screen">
+      <img src={quora} className="w-24 h-7"/>
+      <img src={home} className="w-7 h-7 ml-10 cursor-pointer"/>
+      <img src={clipboard} className="w-7 h-7 ml-10 cursor-pointer"/>
+      <img src={edit} className="w-7 h-7 ml-10 cursor-pointer"/>
+      <img src={group} className="w-7 h-7 ml-10 cursor-pointer"/>
+      <img src={bell} className="w-7 h-7 ml-10 cursor-pointer"/>
+      <div className="flex border border-spacing-1 h-9 ml-10 w-72 p-1 cursor-pointer">
+        <img src={lens} className="w-3 h-3 mt-2 ml-3"/>
+        <input onChange={(e)=> props?.setSearch(e.target.value)} placeholder="Search Quora" className="ml-2 outline-none"/>
+      </div>
+      <h1 className="text-sm border border-spacing-1 rounded-full p-2 ml-5 h-9">Try Quora+</h1>
+      <img src={globe} className="w-5 h-5 ml-5 mt-2 cursor-pointer"/>
+     // in place of true do email validation 
+      {true ? <Avatar round size="25" className="mt-0.5 ml-2 cursor-pointer" name={"to be placed "}/>
+      : <Avatar round size="25" className="mt-0.5 ml-2 cursor-pointer" src={account}/>}
+      <h1 onClick={()=> setPost(true)} className="bg-red-700 rounded-full text-sm cursor-pointer text-white w-36 pl-6 ml-6 pt-2 h-9">Add question</h1>
+      {post && <PostPopup setPost={setPost}/>}
+    </div>
+  )
+}
+
+export default Navbar
